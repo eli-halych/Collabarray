3b7ec39da77134567473a8052504b1037cc06bf9
Godstime Osarobo <nosarobo@gmail.com>
2017-11-04 16:25:52 +0000

    fixed the no icon on the gihub facebook and google sign in buttons problem

--- src/Header.js
+++ src/Header.js
@@ -1,50 +1,54 @@
-import React, { Component } from 'react';
-import './Header.css'
-import 'bootstrap-social';
-
-
+import React, { Component } from "react";
+import "./Header.css";
+import "bootstrap-social";
 
 class Header extends Component {
-
-  render() {
-    return (
-      <div className="Header">
-        
-        <nav className="navbar navbar-default">
-
-            <div className="container-fluid col-sm-6 col-md-6">
-              <div className="navbar-header">
-                <a className="navbar-brand" >Collabarray</a>
-              </div>
-              <ul className="nav navbar-nav">
-                <li className="active"><a >Home</a></li>
-                <li><a >Page 1</a></li>
-                <li><a >Page 2</a></li>
-                <li><a >Page 3</a></li>
-              </ul>
-            </div>
-
-            
-              {this.props.authenticated 
-              ?
-              (<div className="container-fluid col-sm-6 col-md-6">
-              <ul className="nav navbar-nav">
-                <li className="active"><a >Home</a></li>
-                <li><a >Page 1</a></li>
-                <li><a >Page 2</a></li>
-                <li><a >Page 3</a></li>
-              </ul>
-              </div>)
-              :
-              null
-              }
-            
-
-        </nav>
-
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="Header">
+				<nav className="navbar navbar-default">
+					{this.props.authenticated ? (
+						<div className="container-fluid col-sm-6 col-md-6">
+							<ul className="nav navbar-nav">
+								<li className="active">
+									<a>Home</a>
+								</li>
+								<li>
+									<a>Page 1</a>
+								</li>
+								<li>
+									<a>Page 2</a>
+								</li>
+								<li>
+									<a>Page 3</a>
+								</li>
+							</ul>
+						</div>
+					) : (
+						<div className="container-fluid col-sm-6 col-md-6">
+							<div className="navbar-header">
+								<a className="navbar-brand">Collabarray</a>
+							</div>
+							<ul className="nav navbar-nav">
+								<li className="active">
+									<a>Home</a>
+								</li>
+								<li>
+									<a>Page 1</a>
+								</li>
+								<li>
+									<a>Page 2</a>
+								</li>
+								<li>
+									<a>Page 3</a>
+								</li>
+							</ul>
+						</div>
+					)}
+				</nav>
+			</div>
+		);
+	}
 }
 
 export default Header;
